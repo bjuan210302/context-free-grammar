80551522fb70a256276665bfa043a5af1b6ec181
Sebastian Jaramillo <sebastian.jaramillo2@correo.icesi.edu.co>
2021-10-30 17:50:38 -0500

    CYK algorithm base

--- src/code/Grammar.js
+++ src/code/Grammar.js
@@ -22,5 +22,20 @@ class Grammar{
 
         }
     }
+
+    CYKAlgorithm(testString){
+        for (let j = 2; j <= testString.length; j++) {
+            for (let i = 1; i < (testString.length-j+1); i++) {
+                var tuples = {}
+
+                for (let k = 0; k <= j-1; k++) {
+                    const p = this.outputMatrix[i-1][k-1]
+                    const p2 = this.outputMatrix[(i-1)+(k)][(j-1)-(k)]
+                    //TODO: cartesian prod of p and p2
+                }
+                
+            }
+        }
+    }
     
 }
